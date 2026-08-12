08abc5b0c171e4d000d5b53af3138f6643cfe3ce
Viktor Bazatyn <bazatinviktor@gmail.com>
2026-08-12 21:59:20 +0300

    Implement Book model

--- library/book/models.py
+++ library/book/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from author.models import Author
 
 class Book(models.Model):
     """
@@ -15,18 +15,24 @@ class Book(models.Model):
         param authors: list of Authors
         type authors: list->Author
     """
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    count = models.IntegerField(default=10)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         """
         Magic method is redefined to show all information about Book.
         :return: book id, book name, book description, book count, book authors
         """
+        return f"{self.id} {self.name} {self.description} {self.count} {list(self.authors.all())}"
 
     def __repr__(self):
         """
         This magic method is redefined to show class and id of Book object.
         :return: class, id
         """
+        return f"Book(id={self.id})"
 
     @staticmethod
     def get_by_id(book_id):
@@ -34,6 +40,10 @@ class Book(models.Model):
         :param book_id: SERIAL: the id of a Book to be found in the DB
         :return: book object or None if a book with such ID does not exist
         """
+        try:
+            return Book.objects.get(id=book_id)
+        except Book.DoesNotExist:
+            return None
 
     @staticmethod
     def delete_by_id(book_id):
@@ -42,6 +52,13 @@ class Book(models.Model):
         :type book_id: int
         :return: True if object existed in the db and was removed or False if it didn't exist
         """
+        book = Book.get_by_id(book_id)
+
+        if book is None:
+            return False
+
+        book.delete()
+        return True
 
     @staticmethod
     def create(name, description, count=10, authors=None):
@@ -56,6 +73,16 @@ class Book(models.Model):
         type authors: list->Author
         :return: a new book object which is also written into the DB
         """
+        book = Book.objects.create(
+            name=name,
+            description=description,
+            count=count
+        )
+
+        if authors:
+            book.authors.set(authors)
+
+        return book
 
     def to_dict(self):
         """
@@ -69,6 +96,13 @@ class Book(models.Model):
         |   'authors': []
         | }
         """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "count": self.count,
+            "authors": [author.to_dict() for author in self.authors.all()],
+        }
 
     def update(self, name=None, description=None, count=None):
         """
@@ -81,6 +115,16 @@ class Book(models.Model):
         type count: int default=10
         :return: None
         """
+        if name is not None:
+            self.name = name
+
+        if description is not None:
+            self.description = description
+
+        if count is not None:
+            self.count = count
+
+        self.save()
 
     def add_authors(self, authors):
         """
@@ -88,6 +132,7 @@ class Book(models.Model):
         param authors: list authors
         :return: None
         """
+        self.authors.add(*authors)
 
     def remove_authors(self, authors):
         """
@@ -95,9 +140,11 @@ class Book(models.Model):
         param authors: list authors
         :return: None
         """
+        self.authors.remove(*authors)
 
     @staticmethod
     def get_all():
         """
         returns data for json request with QuerySet of all books
         """
+        return Book.objects.all()
